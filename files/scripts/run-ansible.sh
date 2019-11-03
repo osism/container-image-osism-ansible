886582484f0bd4f98178e6dd638d2bbc43435702
Christian Berendt <berendt@betacloud-solutions.de>
2019-11-03 23:20:59 +0100

    Transfer to the public

---
+++ files/scripts/run-ansible.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+ENVIRONMENT=ansible
+
+if [[ $# -lt 1 ]]; then
+    echo usage: osism-$ENVIRONMENT ENVIRONMENT [...]
+    exit 1
+fi
+
+environment=$1
+shift
+
+ANSIBLE_DIRECTORY=/ansible
+CONFIGURATION_DIRECTORY=/opt/configuration
+ENVIRONMENTS_DIRECTORY=$CONFIGURATION_DIRECTORY/environments
+VAULT=${VAULT:-$ENVIRONMENTS_DIRECTORY/.vault_pass}
+
+if [[ -e /ansible/ara.env ]]; then
+    source /ansible/ara.env
+fi
+
+export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/ansible.cfg
+if [[ -e $ENVIRONMENTS_DIRECTORY/$environment/ansible.cfg ]]; then
+    export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/$environment/ansible.cfg
+fi
+
+cd $ENVIRONMENTS_DIRECTORY/$environment
+
+ansible \
+  --vault-password-file $VAULT \
+  -e @$ENVIRONMENTS_DIRECTORY/configuration.yml \
+  -e @$ENVIRONMENTS_DIRECTORY/secrets.yml \
+  -e @secrets.yml \
+  -e @images.yml \
+  -e @configuration.yml \
+  "$@"
---
+++ files/scripts/run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+ENVIRONMENT=run
+
+if [[ $# -lt 2 ]]; then
+    echo usage: osism-$ENVIRONMENT ENVIRONMENT SERVICE [...]
+    exit 1
+fi
+
+environment=$1
+shift
+
+service=$1
+shift
+
+ANSIBLE_DIRECTORY=/ansible
+CONFIGURATION_DIRECTORY=/opt/configuration
+ENVIRONMENTS_DIRECTORY=$CONFIGURATION_DIRECTORY/environments
+VAULT=${VAULT:-$ENVIRONMENTS_DIRECTORY/.vault_pass}
+
+if [[ -e /ansible/ara.env ]]; then
+    source /ansible/ara.env
+fi
+
+export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/ansible.cfg
+if [[ -e $ENVIRONMENTS_DIRECTORY/$environment/ansible.cfg ]]; then
+    export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/$environment/ansible.cfg
+fi
+
+cd $ENVIRONMENTS_DIRECTORY/$environment
+
+ansible-playbook \
+  --vault-password-file $VAULT \
+  -e @$ENVIRONMENTS_DIRECTORY/configuration.yml \
+  -e @$ENVIRONMENTS_DIRECTORY/secrets.yml \
+  -e @secrets.yml \
+  -e @images.yml \
+  -e @configuration.yml \
+  "$@" \
+  playbook-$service.yml
