52fb1a8402e0da9c509c3b9bccfc8e29f9323f9e
Christian Berendt <berendt@osism.tech>
2021-07-14 18:04:41 +0200

    Handle release version
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/src/render-versions.py
+++ files/src/render-versions.py
@@ -20,9 +20,25 @@ environment = jinja2.Environment(loader=loader)
 # render versions.yml
 
 template = environment.get_template("versions.yml.j2")
-result = template.render({
-  'docker_version': versions['osism_projects']['docker']
-})
+
+if VERSION == "latest":
+    result = template.render({
+      'docker_version': versions['osism_projects']['docker'],
+      'version': VERSION
+    })
+else:
+    with open("/release/%s/ceph.yml" % VERSION, "rb") as fp:
+        versions_ceph = yaml.load(fp, Loader=yaml.FullLoader)
+    with open("/release/%s/openstack.yml" % VERSION, "rb") as fp:
+        versions_openstack = yaml.load(fp, Loader=yaml.FullLoader)
+
+    result = template.render({
+      'cephclient': versions_ceph['docker_images']['cephclient'],
+      'docker_version': versions['osism_projects']['docker'],
+      'openstackclient': versions_openstack['docker_images']['openstackclient'],
+      'version': VERSION
+    })
+
 with open("/ansible/group_vars/all/versions.yml", "w+") as fp:
     fp.write(result)
 
