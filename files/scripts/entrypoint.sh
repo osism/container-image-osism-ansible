7cab7fdd70c5eb248b7b8a51fb9504ffad6df9b4
Christian Berendt <berendt@osism.tech>
2021-11-17 20:58:19 +0100

    Copy versions.yml to /interface + add manager_version (#189)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/osism-ansible
+mkdir -p /interface/osism-ansible /interface/versions
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/osism-ansible/
+cp /ansible/group_vars/all/versions.yml /interface/versions/osism-ansible.yml
+
 exec /usr/bin/dumb-init -- "$@"
