1b2a8c4cc3b990af96d6e643d133a5e8fa329fb1
Christian Berendt <berendt@osism.tech>
2021-12-30 18:13:15 +0100

    Add change.sh script (#206)
    With this script it is possible to change a certain
    OSISM collection for the development.
    
    The following exec clones the osism/ansible-collection-services
    repository and then selects the branch main there.
    
    docker exec -u root -it manager_osism-ansible_1 /change.sh services main
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ files/scripts/change.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ ! -e /usr/bin/git ]]; then
+  apt-get update \
+    && apt-get install --no-install-recommends -y git
+fi
+
+rm -rf /ansible/collections/ansible_collections/osism/$1
+git clone --depth 1 -b $2 https://github.com/osism/ansible-collection-services /ansible/collections/ansible_collections/osism/$1
