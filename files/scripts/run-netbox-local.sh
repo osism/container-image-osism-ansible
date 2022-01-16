731f8973f6499a528edc229ed02657aadd860a42
Christian Berendt <berendt@osism.tech>
2022-01-16 21:03:42 +0100

    Improve the run-netbox scripts (#211)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/run-netbox-local.sh
+++ files/scripts/run-netbox-local.sh
@@ -12,23 +12,17 @@ shift
 
 ANSIBLE_DIRECTORY=/ansible
 CONFIGURATION_DIRECTORY=/opt/configuration
-ENVIRONMENTS_DIRECTORY=$CONFIGURATION_DIRECTORY/environments
 NETBOX_TOKEN=$(< /run/secrets/NETBOX_TOKEN)
 
 if [[ -e /ansible/ara.env ]]; then
     source /ansible/ara.env
 fi
 
-export ANSIBLE_INVENTORY=$CONFIGURATION_DIRECTORY/$ENVIRONMENT/inventory
-export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/ansible.cfg
-if [[ -e $CONFIGURATION_DIRECTORY/$ENVIRONMENT/ansible.cfg ]]; then
-    export ANSIBLE_CONFIG=$CONFIGURATION_DIRECTORY/$ENVIRONMENT/ansible.cfg
-fi
-
 cd $CONFIGURATION_DIRECTORY/$ENVIRONMENT
 
 ansible-playbook \
+  -i localhost, \
   -e netbox_token=$NETBOX_TOKEN \
   -e netbox_url=$NETBOX_API \
   "$@" \
-  $CONFIGURATION_DIRECTORY/$ENVIRONMENT/$playbook.yml
+  $CONFIGURATION_DIRECTORY/$ENVIRONMENT/playbooks/$playbook.yml
