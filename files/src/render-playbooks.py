3d7b37750676a87f3c6360600539e0aab0cc13d1
Christian Berendt <berendt@osism.tech>
2023-02-05 15:00:52 +0100

    Fix name of 2 plays
    network & operator should have the manager- prefix.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/src/render-playbooks.py
+++ files/src/render-playbooks.py
@@ -11,12 +11,23 @@ PREFIXES = [
     "infrastructure",
 ]
 
+KEEP_PREFIX = {
+    "generic": [],
+    "manager": ["operator", "network"],
+    "monitoring": [],
+    "infrastructure": [],
+}
+
 result = {}
 
 for prefix in PREFIXES:
     for path in Path("/ansible").glob(f"{prefix}-*.yml"):
         name = path.with_suffix("").name[len(prefix) + 1 :]  # noqa E203
-        result[name] = prefix
+
+        if name in KEEP_PREFIX[prefix]:
+            result[f"{prefix}-{name}"] = prefix
+        else:
+            result[name] = prefix
 
 with open("/ansible/playbooks.yml", "w+") as fp:
     fp.write(yaml.dump(result))
