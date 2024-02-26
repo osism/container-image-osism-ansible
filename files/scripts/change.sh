7e63e193664c2ccf423d767c3fa84d6cf206da8d
Christian Berendt <berendt@osism.tech>
2024-02-26 09:17:35 +0100

    Merge change scripts
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/change.sh
+++ files/scripts/change.sh
@@ -5,5 +5,21 @@ if [[ ! -e /usr/bin/git ]]; then
     && apt-get install --no-install-recommends -y git
 fi
 
-rm -rf /ansible/collections/ansible_collections/osism/$1
-git clone --depth 1 -b $2 https://github.com/osism/ansible-collection-$1 /ansible/collections/ansible_collections/osism/$1
+if [[ "$1" == "osism" ]]; then
+    rm -rf /python-osism
+    git clone --depth 1 -b $2 https://github.com/osism/python-osism /python-osism
+
+    pushd /python-osism
+    pip3 uninstall -y osism
+    python3 -m pip --no-cache-dir install -U /python-osism
+    popd
+elif [[ "$1" == "playbooks" ]]; then
+    rm -rf /playbooks
+    git clone --depth 1 -b $2 https://github.com/osism/ansible-playbooks /playbooks
+
+    python3 /src/render-playbooks.py
+    cp /ansible/playbooks.yml /interface/playbooks/osism-ansible.yml
+else
+    rm -rf /ansible/collections/ansible_collections/osism/$1
+    git clone --depth 1 -b $2 https://github.com/osism/ansible-collection-$1 /ansible/collections/ansible_collections/osism/$1
+fi
